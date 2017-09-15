5cf3b0a6beb7eed180081192468a1881cbd0d374
android23235616 <majumdartanmay68@gmail.com>
2017-09-16 00:53:18 +0530

    smileys

--- app/src/main/java/com/fsingh/pranshooverma/foodsingh/place_order_activity.java
+++ app/src/main/java/com/fsingh/pranshooverma/foodsingh/place_order_activity.java
@@ -364,17 +364,14 @@ public class place_order_activity extends AppCompatActivity implements Navigatio
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.menu) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+            startActivity(new Intent(getApplicationContext(),menu.class));
+        } else if (id == R.id.cart) {
+            startActivity(new Intent(getApplicationContext(),cart.class));
+        } else if (id == R.id.orders) {
+            startActivity(new Intent(getApplicationContext(),order_history.class));
+        } else if (id == R.id.SignOut) {
 
         }
 
