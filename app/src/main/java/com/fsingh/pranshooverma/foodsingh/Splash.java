e44ec0d8e58ea1c675ff342be0a7e935156de5fa
android23235616 <majumdartanmay68@gmail.com>
2017-09-18 01:31:21 +0530

    CollapsingToolBar Added2 pushing pushing2 pushin3 push4
    new changed

--- app/src/main/java/com/fsingh/pranshooverma/foodsingh/Splash.java
+++ app/src/main/java/com/fsingh/pranshooverma/foodsingh/Splash.java
@@ -2,12 +2,34 @@ package com.fsingh.pranshooverma.foodsingh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash);
+        ImageView AnimationTarget = (ImageView)findViewById(R.id.progressBar);
+       // Glide.with(this).load(R.drawable.signin).into(AnimationTarget);
+        RotateAnimation rotate = new RotateAnimation(0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        rotate.setDuration(2500);
+        rotate.setInterpolator(new LinearInterpolator());
+        rotate.setRepeatCount(Animation.INFINITE);
+        AnimationTarget.setAnimation(rotate);
     }
 }
