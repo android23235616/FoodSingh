d4ec69f1eaec9489dd03e6936829a17978a8ba13
Soumya Deb <soumyadeb1809@gmail.com>
2017-10-18 20:42:03 +0530

    - FCM Added

---
+++ app/src/main/java/com/fsingh/pranshooverma/foodsingh/FirebaseMessagingService.java
@@ -0,0 +1,35 @@
+package com.fsingh.pranshooverma.foodsingh;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Soumya Deb on 18-10-2017.
+ */
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String notificationTitle = remoteMessage.getNotification().getTitle();
+        String notificationBody = remoteMessage.getNotification().getBody();
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(notificationTitle)
+                        .setContentText(notificationBody);
+
+
+
+        int mNotificationId = (int)System.currentTimeMillis();
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+}
