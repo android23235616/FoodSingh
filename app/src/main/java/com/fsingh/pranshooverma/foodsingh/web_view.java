2adbc2c6d3f2d1265a4e4cc39d14003817a64af8
android23235616 <majumdartanmay68@gmail.com>
2017-10-21 07:06:49 +0530

    kuchbhi

---
+++ app/src/main/java/com/fsingh/pranshooverma/foodsingh/web_view.java
@@ -0,0 +1,158 @@
+package com.fsingh.pranshooverma.foodsingh;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class web_view extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    WebView  browser;
+    String url;
+    ProgressDialog progress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+////////////////////////////////////////////////////////////////////////////////////////////////////
+        //CODING
+
+        initialize();
+
+        if(checking_net_permission())
+        {
+
+
+            browser.getSettings().setJavaScriptEnabled(true);
+            browser.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+            browser.clearCache(true);
+            browser.getSettings().setBuiltInZoomControls(true);
+
+            browser.setWebViewClient(new web_view_class()
+            {
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon)
+                {
+                    progress.setMessage("Loading");
+                    progress.show();
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    if(progress.isShowing())
+                    {
+                        progress.dismiss();
+                    }
+                }
+            });
+
+
+            browser.loadUrl(url);
+        }
+
+    }
+
+
+
+    private void initialize() {
+        browser=(WebView)findViewById(R.id.webView);
+        Bundle b=getIntent().getExtras();
+        url=b.getString("url");
+        progress=new ProgressDialog(this);
+    }
+    private Boolean checking_net_permission() {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+        if(browser.canGoBack())
+        {
+            browser.goBack();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.web_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
