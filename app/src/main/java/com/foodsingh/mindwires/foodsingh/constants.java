55c9cae4d923f0f345e2c30c520d599e7e9b832e
android23235616 <majumdartanmay68@gmail.com>
2018-02-24 01:53:37 +0530

    Bull Push

--- app/src/main/java/com/foodsingh/mindwires/foodsingh/constants.java
+++ app/src/main/java/com/foodsingh/mindwires/foodsingh/constants.java
@@ -25,31 +25,31 @@ public class constants {
     public static final String foodsingh = "kashd'd70]s";
     public static  final String all_data_cart="";
 
-    public static final String check_mobile="http://foodsingh.com/app_files/usercheck_mobile.php";
+    public static final String check_mobile="http://foodsingh.com/app_files/user/check_mobile.php";
 
     public static final String main_url = "http://foodsingh.com/app_files/user/get_data.php";
-    public static final String service_status_url = "http://foodsingh.com/app_files/userget_service_status.php";
-    public static final String token_url = "http://foodsingh.com/app_files/userupdate_token.php";
-    public static final String order_details = "http://foodsingh.com/app_files/userget_order_details.php";
+    public static final String service_status_url = "http://foodsingh.com/app_files/user/get_service_status.php";
+    public static final String token_url = "http://foodsingh.com/app_files/user/update_token.php";
+    public static final String order_details = "http://foodsingh.com/app_files/user/get_order_details.php";
 
     //yahan pe chnange kr
-    public static String login_details_url="http://foodsingh.com/app_files/userlogini_save_and_check.php";
-    public static String login_check_url="http://foodsingh.com/app_files/userlogin.php";
+    public static String login_details_url="http://foodsingh.com/app_files/user/logini_save_and_check.php";
+    public static String login_check_url="http://foodsingh.com/app_files/user/login.php";
 
-    public static String coupon_url="http://foodsingh.com/app_files/usercheck_coupon3.php";
-    public  static  String get_categories="http://foodsingh.com/app_files/userget_categories2.php";
+    public static String coupon_url="http://foodsingh.com/app_files/user/check_coupon3.php";
+    public  static  String get_categories="http://foodsingh.com/app_files/user/get_categories2.php";
     public static int min_order=-1;
-    public static String remove_coupon="http://foodsingh.com/app_files/userremove_coupon.php";
-    public static String get_menu_category_wise="http://foodsingh.com/app_files/userget_menu.php";
-    public static String update_password="http://foodsingh.com/app_files/userupdate_password.php";
-    public static String send_to_debian="http://foodsingh.com/app_files/userplace_order.php";
-    public static String order_history="http://foodsingh.com/app_files/userget_order_history.php";
-    public static String discount_coupon="http://foodsingh.com/app_files/userget_discount.php";
-    public static String forgot_password="http://foodsingh.com/app_files/userforgot_password.php";
-    public static String update_details="http://foodsingh.com/app_files/userupdate_user_data.php";
-    public static String get_details="http://foodsingh.com/app_files/userget_user_data.php";
+    public static String remove_coupon="http://foodsingh.com/app_files/user/remove_coupon.php";
+    public static String get_menu_category_wise="http://foodsingh.com/app_files/user/get_menu.php";
+    public static String update_password="http://foodsingh.com/app_files/user/update_password.php";
+    public static String send_to_debian="http://foodsingh.com/app_files/user/place_order.php";
+    public static String order_history="http://foodsingh.com/app_files/user/get_order_history.php";
+    public static String discount_coupon="http://foodsingh.com/app_files/user/get_discount.php";
+    public static String forgot_password="http://foodsingh.com/app_files/user/forgot_password.php";
+    public static String update_details="http://foodsingh.com/app_files/user/update_user_data.php";
+    public static String get_details="http://foodsingh.com/app_files/user/get_user_data.php";
 
-    public static String get_service_status="http://foodsingh.com/app_files/userget_service_status.php";
+    public static String get_service_status="http://foodsingh.com/app_files/user/get_service_status.php";
     public static String logTag = "android23235616";
 
     public static List<String> items_name=new ArrayList<>();
@@ -60,9 +60,9 @@ public class constants {
     public static List<String> item_name_deb=new ArrayList<>();
     public static List<String>  item_quant_deb=new ArrayList<>();
 
-    public static String set_version="http://foodsingh.com/app_files/userset_version.php";
+    public static String set_version="http://foodsingh.com/app_files/user/set_version.php";
 
-    public static String get_sides="http://foodsingh.com/app_files/userget_sides.php";
+    public static String get_sides="http://foodsingh.com/app_files/user/get_sides.php";
 
 
 }
